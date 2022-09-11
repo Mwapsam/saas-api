6f9044c2dafee5157510dd3015023b2561ceddea
Mwapsam <mwapsam@gmail.com>
2022-09-11 16:30:00 +0200

    Implement create actions on track and category controllers

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -3,4 +3,19 @@ class Api::V1::CategoriesController < ApplicationController
         categories = Category.all
         render json: categories, status: 200
     end
+
+    def create
+        category = Category.new(category_params)
+        if category.save
+            render json: category, status: 200
+        else
+            render json: {errors: "Category was not created!"}, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:name, :decription, :image)
+    end
 end
--- app/controllers/api/v1/tracks_controller.rb
+++ app/controllers/api/v1/tracks_controller.rb
@@ -3,4 +3,20 @@ class Api::V1::TracksController < ApplicationController
         tracks = Track.all
         render json: tracks, status: 200
     end
+
+    def create
+        category = Category.find(params[:id])
+        track = category.tracks.new(track_params)
+        if track.save
+            render json: track, status: 200
+        else
+            render json: {errors: "Track was not created!"}, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def track_params
+        params.require(:track).permit(:name, :artist, :year, :public, :category_id, :image)
+    end
 end
