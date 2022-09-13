5e36e4eb86357ffcabcb8ea810a5f19982eca293
Mwapsam <mwapsam@gmail.com>
2022-09-13 09:25:01 +0200

    Add audio file to track serializer

--- app/serializers/track_serializer.rb
+++ app/serializers/track_serializer.rb
@@ -1,9 +1,14 @@
 class TrackSerializer < ActiveModel::Serializer
-  attributes :id, :name, :artist, :year, :public, :category_id, :image
+  include Rails.application.routes.url_helpers
+  attributes :id, :name, :artist, :year, :public, :category_id, :image, :audio
 
   belongs_to :category
 
   def image
     rails_blob_path(object.image, only_path: true) if object.image.attached?
+  end
+
+  def audio
+    rails_blob_path(object.audio, only_path: true) if object.audio.attached?
   end
 end
