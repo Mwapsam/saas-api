143233bc66d947afb63767ece8390c2e49b19f87
Mwapsam <mwapsam@gmail.com>
2022-09-13 09:23:57 +0200

    Modify tracks API endpoint to fix the bug

--- app/controllers/api/v1/tracks_controller.rb
+++ app/controllers/api/v1/tracks_controller.rb
@@ -1,12 +1,13 @@
 class Api::V1::TracksController < ApplicationController
+    before_action :set_track, only: %i[update destroy]
+
     def index
         tracks = Track.all
         render json: tracks, status: 200
     end
 
     def create
-        category = Category.find(params[:id])
-        track = category.tracks.new(track_params)
+        track = Track.new(track_params)
         if track.save
             render json: track, status: 200
         else
@@ -14,9 +15,30 @@ class Api::V1::TracksController < ApplicationController
         end
     end
 
+    def update
+        if @track.update(track_params)
+            render json: @track
+        else
+            render json: @track.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @track = Track.find(params[:id])
+        if @track.destroy
+            render json: { message: 'Track has been successfully deleted' }
+        else
+            render json: @track.errors, status: :unprocessable_entity
+        end
+    end
+
     private
 
+    def set_track
+        @track = Track.find(params[:id])
+    end
+
     def track_params
-        params.require(:track).permit(:name, :artist, :year, :public, :category_id, :image)
+        params.require(:track).permit(:name, :artist, :year, :public, :category_id, :image, :audio)
     end
 end
